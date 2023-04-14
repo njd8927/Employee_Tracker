e0f78c0f1b5b16592c38a6c77b6524a7677d0997
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-13 22:53:02 -0400

    created schema & seeds sql files & created db connection to server

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name) VALUES 
+('accounting'), 
+('legal'),
+('Human Resources'),
+('Sales'),
+('Customer Service');
+
+
+INSERT INTO role (title, salary) VALUES
+('Controller', 150000),
+('Senior Counsel', 300000),
+('HR Manager', 100000),
+('VP of Sales', 125000),
+('CS Manager', 70000);
+
+
+INSERT INTO employee (first_name, last_name, manager_id) VALUES
+('Chrissy', 'Zentai', 1),
+('Nick', 'Zentai', 2),
+('Riley', 'Zentai', 3),
+('Mike', 'Zentai', 4),
+('Zsolt', 'Zentai', 5)
+
---
+++ server.js
@@ -0,0 +1,46 @@
+const mySql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mySql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'pass123',
+        database: 'employee_tracker_db'
+    },
+    console.log('Connected to employee_tracker_db database!')
+);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+
+function landingPage () {
+    inquirer.prompt([
+        {
+            name: "options",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add A Department",
+                "Add A Role",
+                "Add An Employee",
+                "Update An Employee Role"
+            ],
+        }
+    ]);
+};
+
+
