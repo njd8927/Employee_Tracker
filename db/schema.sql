e7bcdd48dcb68a132ed1acfdb7a57b2af5647219
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-13 23:18:14 -0400

    updated table data

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ USE employee_tracker_db;
 
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
+    salary INT,
     department_id INT
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -25,9 +25,11 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
     FOREIGN KEY (role_id)
     REFERENCES role(id)
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
 
