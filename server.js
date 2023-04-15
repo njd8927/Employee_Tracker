00d17902a8ba05ca8c22fb2d9bf0b1564249582f
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-14 21:47:33 -0400

    fixed syntax error in server.js

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ const connection = mySql.createConnection(
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+});
 
-function landingPage () {
+function landingPage() {
     inquirer.prompt([
         {
             name: "options",
@@ -43,40 +43,40 @@ function landingPage () {
             ],
         }
     ])
-    .then((result) => {
-        console.log(result.options);
+        .then((result) => {
+            console.log(result.options);
 
-    switch (result.options) {
-        case "View Departments":
-            viewDepartment();
-            break;
-        case "View Roles":
-            viewRole();
-            break;
-        case "View Employees":
-            viewEmployee();
-            break;
-        case "Add Department":
-            addDepartment();
-            break;
-        case "Add Role":
-            addRole();
-            break;
-        case "Add Employee":
-            addEmployee();
-            break;
-        case "Update Employee Role":
-            updateEmployee();
-            break;    
-    }
-    });
+            switch (result.options) {
+                case "View Departments":
+                    viewDepartment();
+                    break;
+                case "View Roles":
+                    viewRole();
+                    break;
+                case "View Employees":
+                    viewEmployee();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployee();
+                    break;
+            }
+        });
 };
 
 landingPage();
 
 function viewDepartment() {
-   let query = "SELECT * FROM department";
-    connection.query(query,function (err,res) {
+    let query = "SELECT * FROM department";
+    connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
         landingPage();
@@ -85,7 +85,7 @@ function viewDepartment() {
 
 function viewRole() {
     let query = "SELECT * FROM role";
-    connection.query(query, function (err,res) {
+    connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
         landingPage();
@@ -94,11 +94,11 @@ function viewRole() {
 
 function viewEmployee() {
     let query = "SELECT * FROM employee";
-    connection.query(query, function (err,res) {
-            if (err) throw err;
-            console.table(res);
-            landingPage();
-        });
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        landingPage();
+    });
 };
 
 function addDepartment() {
@@ -108,8 +108,8 @@ function addDepartment() {
             type: "input",
             message: "What is the name of the department you would like to add?"
         }
-    ]).then(function(choice){
-        connection.query("INSERT INTO department (name) VALUES (?)", [choice.deptName], function (err,res) {
+    ]).then(function (choice) {
+        connection.query("INSERT INTO department (name) VALUES (?)", [choice.deptName], function (err, res) {
             if (err) throw err;
             console.table(res);
             landingPage();
@@ -134,8 +134,8 @@ function addRole() {
             type: "input",
             message: "Please enter the department id that the role will be assigned to."
         }
-    ]).then(function(choice){
-        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [choice.roleTitle, choice.salary, choice.departmentID], function (err,res) {
+    ]).then(function (choice) {
+        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [choice.roleTitle, choice.salary, choice.departmentID], function (err, res) {
             if (err) throw err;
             console.table(res);
             landingPage();
@@ -150,23 +150,23 @@ function addEmployee() {
             type: "input",
             message: "What is the first name of the employee?"
         },
-         {
+        {
             name: "empLastName",
             type: "input",
             message: "What is the last name of the employee?"
         },
-         {
+        {
             name: "empRoleID",
             type: "input",
             message: "What is the employees role id number?"
         },
-         {
+        {
             name: "empManagerID",
             type: "input",
             message: "What is the employees managers id number?"
         },
-    ]).then(function(choice){
-        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [choice.empFirstName, choice.empLastName, choice.empRoleID, choice.empManagerID], function (err,res) {
+    ]).then(function (choice) {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [choice.empFirstName, choice.empLastName, choice.empRoleID, choice.empManagerID], function (err, res) {
             if (err) throw err;
             console.table(res);
             landingPage();
@@ -186,8 +186,8 @@ function updateEmployee() {
             type: "input",
             message: "What is the role id you would like to update the employee to?"
         },
-    ]).then(function(choice){
-        connection.query("UPDATE employee SET role_id=? WHERE first_name=?", [choice.empName, choice.roleIDUpdate], function (err,res) {
+    ]).then(function (choice) {
+        connection.query("UPDATE employee SET role_id=? WHERE first_name=?", [choice.empName, choice.roleIDUpdate], function (err, res) {
             if (err) throw err;
             console.table(res);
             landingPage();
